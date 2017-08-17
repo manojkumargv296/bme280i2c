ec69b44f9b9ee10c4dce3c4a81853fed71ab4575
manojkumargv296 <31094979+manojkumargv296@users.noreply.github.com>
2017-08-17 14:40:07 +0530

    Add files via upload

---
+++ Bme280.ino
@@ -0,0 +1,31 @@
+#include <BME280I2C.h>
+#include <Wire.h>             
+ float temp, hum, pres;
+BME280I2C bme;                 
+
+
+void setup() {
+  Serial.begin(9600);
+
+bme.begin();
+}
+
+void loop() {
+ 
+
+ 
+                                           
+   bme.read(pres, temp, hum);   
+   
+
+  Serial.print("Temp: ");
+  Serial.print(temp);
+  Serial.print("C");
+  Serial.print("\t\tHumidity: ");
+  Serial.print(hum);
+  Serial.print("% RH");
+  Serial.print("\t\tPressure: ");
+  Serial.print(pres);
+  Serial.println("Bar");
+  delay(1000);
+}
